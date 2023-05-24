909f176e29e2b3efc060fa78125373510b0556de
w0nderland05 <111275210+w0nderland05@users.noreply.github.com>
2023-05-24 21:19:23 +0900

    비밀번호 패턴체크 구현중

---
+++ src/main/java/org/boardpj/commons/validators/PasswordValidator.java
@@ -0,0 +1,33 @@
+package org.boardpj.commons.validators;
+
+public interface PasswordValidator {
+    /**
+     * 비밀번호 형식 체크 - 알파벳 체크
+     * @param password
+     * @param caseIncentive
+     *          false:소문자 +대문자 반드시 포함되는 패턴
+     *          true: 대소문자 상관없이 포함되는 패턴
+     * @return
+     */
+    default boolean alphaCheck(String password, boolean caseIncentive){
+        return false;
+    }
+
+    /**
+     * 숫자가 포함된 패턴인지 체크
+     * @param password
+     * @return
+     */
+    default boolean numCheck(String password){
+        return false;
+    }
+
+    /**
+     * 특수문자가 포함된 패턴인지 체크
+     * @param password
+     * @return
+     */
+    default boolean specialCharsCheck(String password){
+        return false;
+    }
+}
