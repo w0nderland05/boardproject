624f73116d6922b32db7673e0affdc9bad380be9
w0nderland05 <111275210+w0nderland05@users.noreply.github.com>
2023-06-11 17:21:41 +0900

    사이트 설정공통기능 추가

--- src/main/java/org/boardpj/configs/Interceptors/SiteConfigInterceptor.java
+++ src/main/java/org/boardpj/configs/Interceptors/SiteConfigInterceptor.java
@@ -20,22 +20,22 @@ import java.util.Map;
 public class SiteConfigInterceptor implements HandlerInterceptor {
 
     private final ConfigInfoService infoService;
+    private final HttpServletRequest request;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         /** 사이트 설정 조회 */
         Map<String, String> siteConfigs = infoService.get("siteConfig", new TypeReference<Map<String, String>>() {});
-        if (siteConfigs == null) {
-            siteConfigs = new HashMap<>();
-            siteConfigs.put("siteTitle", "");
-            siteConfigs.put("siteDescription", "");
-            siteConfigs.put("cssJsVersion", "" + 1);
-            siteConfigs.put("joinTerms", "");
-        }
-
         request.setAttribute("siteConfig", siteConfigs);
 
         return true;
+    }
+
+    public String get(String name) {
+        Map<String, String> siteConfig = (Map<String, String>)request.getAttribute("siteConfig");
+        String value = siteConfig == null ? "" : siteConfig.get(name);
+
+        return value;
     }
 }
